29c51a0f64c02e63933c5f1b1879194048cc9724
Modurotolu Olokode <durolawk@gmail.com>
2020-07-20 00:02:04 +0100

    setup knex for testing

--- knexfile.js
+++ knexfile.js
@@ -22,33 +22,27 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/test.db3'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 
   production: {
     client: 'pg',
-    // connection: {
-    //   database: 'my_db',
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations'

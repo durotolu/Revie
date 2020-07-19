e505cf175155451b7d2c41c0ead7647d738af6d6
Modurotolu Olokode <durolawk@gmail.com>
2020-07-20 00:03:37 +0100

    tested server returns 200

--- api/server.spec.js
+++ api/server.spec.js
@@ -4,7 +4,11 @@ const server = require('./server');
 describe('server', () => {
   describe('[GET] / endpoint', () => {
     test('the db env is using testing', () => {
-      expect(process.env.NODE_ENV).toBe('testing')
-  });
+      expect(process.env.NODE_ENV).toBe('testing');
+    });
+    test('should return 200 OK', async () => {
+      const response = await request(server).get('/');
+      expect(response.status).toBe(200);
+    })
   })
 })

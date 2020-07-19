64b838e4937d02cf6409205b6f27a9746cd8cda5
Modurotolu Olokode <durolawk@gmail.com>
2020-07-20 00:10:33 +0100

    test register works

--- user/auth-router.spec.js
+++ user/auth-router.spec.js
@@ -1,15 +1,33 @@
 const request = require('supertest');
 const server = require('../api/server');
-const db = require('../database/dbConfig')
+const db = require('../database/dbConfig');
 
 beforeAll(async () => {
-    await db('users').truncate();
-})
+  await db('users').truncate();
+});
 
 let token;
 
 const input = {
-    username: 'testing',
-    password: 'testing',
-    email: 'testing@gmail.com'
-}
+  username: 'testing',
+  password: 'testing',
+  email: 'testing@gmail.com'
+};
+
+describe('users authorization', () => {
+  describe('POST /users', () => {
+    test('should return 201, with testing as user and correct content-type', () => {
+      return request(server)
+        .post('/auth/register')
+        .send(input)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .then(res => {
+          (res.body.username, 'testing');
+        });
+
+      
+    });
+  })
+})
